8eadd2a4a8822316a0181a3912218c8239c79e51
Swoop86dk <cph-mr373@cphbusiness.dk>
2018-03-19 11:28:39 +0100

    tilføjet help

---
+++ src/main/java/PresentationLayer/Help.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PresentationLayer;
+
+import FunctionLayer.LogicFacade;
+import FunctionLayer.LoginSampleException;
+import FunctionLayer.User;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Martin
+ */
+public class Help extends Command
+{
+    @Override
+    String execute( HttpServletRequest request, HttpServletResponse response ) throws LoginSampleException {
+        return "help";
+}
+}
